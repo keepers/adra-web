9dbacceaa83f8546f8faaef3f11070b93dc35f65
ndelvalle <nicolas.delvalle@gmail.com>
2015-11-02 00:02:38 -0300

    Added socket.on('stats') listener to overwrite graph data

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -6,22 +6,14 @@
   .controller('BeneficiaryController', ['$scope', 'api', 'socket', function ($scope, api, socket) {
 
     api.beneficiaries.stats().then(function (beneficiaries) {
-      console.log(beneficiaries);
-
       $scope.data = [beneficiaries.totalDisables, beneficiaries.totalElders, beneficiaries.totalPregnants, beneficiaries.totalLactantMothers];
-
-      console.log($scope.data);
     });
 
-    socket.on('emit~bar', function () {
-      $scope.bar = true;
+    socket.on('stats', function (beneficiaries) {
+      $scope.data = [beneficiaries.totalDisables, beneficiaries.totalElders, beneficiaries.totalPregnants, beneficiaries.totalLactantMothers];
     });
 
-
-    //$scope.data = [2, 3, 30, 10];
     $scope.labels = ['Discapacitados', 'Ancianos', 'Madres embarazadas', 'Madres Lactantes'];
-
-
 
   }]);
 
