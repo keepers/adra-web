26f3cf6434bdb68d5ed14428ff96288d45cafad2
ndelvalle <nicolas.delvalle@gmail.com>
2016-01-20 01:14:50 +0100

    Added process.env.API_URL variable to config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
-    PORT: process.env.PORT || 8080,
-    ENV: process.env.NODE_ENV || 'development'
+  PORT: process.env.PORT || 8080,
+  ENV: process.env.NODE_ENV || 'development',
+  API_URL: process.env.API_URL || 'http://localhost:9090'
 };
